a83792281f7e8446e82a7fce0245d28eac18cd96
erik <eanilsson88@gmail.com>
2025-10-06 16:11:19 +0200

    TODO about trading step

--- src/model/states/StartOfTurnState.java
+++ src/model/states/StartOfTurnState.java
@@ -7,6 +7,6 @@ public class StartOfTurnState extends GameState {
     public GameState run(Model model) {
         model.getScreenHandler().println("Turn " + model.getTurn());
         model.drawBoard();
-        return new PlayerActionState();
+        return new PlayerActionState(); // TODO: On turn 2 and forward, Trading Step
     }
 }
