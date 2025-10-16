00da2a8920daac83757d03ee379ecf7a49e3d8ea
erik <eanilsson88@gmail.com>
2025-10-16 05:22:32 +0200

    Fix game end on turn 7 bug.

--- src/model/states/StepToNextTurnState.java
+++ src/model/states/StepToNextTurnState.java
@@ -7,7 +7,7 @@ public class StepToNextTurnState extends GameState {
     @Override
     public GameState run(Model model) {
         model.incrementTurn();
-        if (model.getTurn() == GameTracks.MAX_GAME_TURN) {
+        if (model.getTurn() > GameTracks.MAX_GAME_TURN) {
             return new DefaultEndingState();
         }
         return new StartOfTurnState();
