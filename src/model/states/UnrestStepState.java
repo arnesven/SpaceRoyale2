c5ea909d7eaf3b591b9d8c7835fb7d485765b96e
erik <eanilsson88@gmail.com>
2025-10-18 07:52:36 +0200

    Battle chance added to unrest step.

--- src/model/states/UnrestStepState.java
+++ src/model/states/UnrestStepState.java
@@ -1,6 +1,8 @@
 package model.states;
 
 import model.Model;
+import model.cards.alignment.AlignmentCard;
+import model.cards.alignment.RebelAlignmentCard;
 import model.cards.events.OppresiveBureaucracyEventCard;
 import util.MyLists;
 import util.MyRandom;
@@ -8,19 +10,30 @@ import util.MyRandom;
 public class UnrestStepState extends GameState {
     @Override
     public GameState run(Model model) {
-        println(model, "Unrest step - the die is rolled.");
         int dieRoll = MyRandom.rollD10();
+        int modified = dieRoll;
+        println(model, "Unrest step - the die is rolled, it's a " + dieRoll + ".");
+        String extra = "";
+        AlignmentCard alignmentCard = model.drawBattleChanceCard();
+        println(model, "Drawing Battle Chance card, it's " + alignmentCard.getName() + ".");
+        if (alignmentCard instanceof RebelAlignmentCard) {
+            extra += " +3";
+            modified += 3;
+        }
         if (MyLists.any(model.getEventCardsInPlay(),
                 ev -> ev instanceof OppresiveBureaucracyEventCard)) {
-            print(model, "The result is " + dieRoll +  " +1 (Oppressive Bureaucarcy), ");
-            dieRoll += 1;
-        } else {
+            extra += " +1 (Oppressive Bureaucarcy)";
+            modified += 1;
+        }
+        if (extra.isEmpty()) {
             print(model, "The result is " + dieRoll + ", ");
+        } else {
+            print(model, "The result is " + dieRoll + extra + " = " + modified + ", ");
         }
 
-        if (dieRoll <= 4) {
+        if (modified <= 5) {
             println(model, "no effect.");
-        } else if (dieRoll <= 9) {
+        } else if (modified <= 9) {
             println(model, "Unrest increases by one.");
             model.increaseUnrest(1);
         } else { // 10
